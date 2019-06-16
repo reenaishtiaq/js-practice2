213ae7ec065c46a79ef60829d6e7c14c87f30df3
reenaishtiaq <49521474+reenaishtiaq@users.noreply.github.com>
2019-06-16 23:29:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function chooseEasy() {
+    alert('hellow, this is level EASY')
+}
+function chooseMedium() {
+    alert('hellow, this is level MEDIUM')
+}
+function chooseHard() {
+    alert('hellow, this is level HARD')
+}
